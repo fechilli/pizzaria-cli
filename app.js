92f33d892e47337c801bca281203176b304c8fcb
fechilli <luizfdas@outlook.com>
2023-01-17 23:55:54 -0300

    aula dia 16/01

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-// 1 - Importar o express
-const express = require('express');
-const path = require('path');
+// 1 - importar o express
+
+const express = require("express");
+const path = require('path')
 
 // 2 - Criar o servidor
+
 const servidor = express();
+servidor.use(express.static(path.join(__dirname,"public")))
 
-// Define a pasta public como sendo a pasta arquivos estáticos
-servidor.use(express.static(path.join(__dirname, 'public')))
+//3 - Definir de uma rota neste servidor
 
-// 3 - Definir de uma rota neste servidor
-// endereço, método, função callback (a ação que o servidor vai realizar quando req chegar)
-servidor.get('/', (req, res)=>{
-    return res.sendFile(__dirname + "/views/index.html");
-});
-
-servidor.get('/carrinho',(req, res)=>{
-    return res.sendFile(__dirname + "/views/carrinho.html");
+// end, metoro, funcao callback 
+servidor.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html")
+    console.log("acessado")
 })
 
-servidor.get('/perfil', (req, res)=>{
-    return res.sendFile(__dirname + "/views/perfil.html");
-});
+servidor.get("/carrinho", (req, res)=>{
+    res.sendFile(__dirname + "/views/carrinho.html")
+    console.log("acessado")
+})
 
-// 4 - Por o servidor no modo "aguardando requisição"
-servidor.listen(3000);
+
+servidor.get("/perfil", (req, res)=>{
+    res.sendFile(__dirname + "/views/perfil2.html")
+    console.log("acessado")
+})
+
+//4 - colocar no modo "aguarando Requisição"
+servidor.listen(3000)
